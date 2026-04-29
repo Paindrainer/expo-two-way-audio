1786c96a668be34b2d409886b6dbc3f8e19f4bcb
André Larsson <larsson.andre@gmail.com>
2026-04-29 15:20:35 +0200

    Revert "Use voice profile for Bluetooth SCO/BLE headsets so call audio rides STREAM_VOICE_CALL."
    This reverts commit 99506bde486d6155b1f21b52cdc261f251503fae.

--- android/src/main/java/expo/modules/twowayaudio/AudioEngine.kt
+++ android/src/main/java/expo/modules/twowayaudio/AudioEngine.kt
@@ -110,9 +110,7 @@ class AudioEngine (context: Context, initialPlaybackSampleRate: Int = DEFAULT_PL
     private fun shouldUseVoiceProfile(deviceType: Int?): Boolean {
         return when (deviceType) {
             AudioDeviceInfo.TYPE_BUILTIN_SPEAKER,
-            AudioDeviceInfo.TYPE_BUILTIN_EARPIECE,
-            AudioDeviceInfo.TYPE_BLUETOOTH_SCO,
-            AudioDeviceInfo.TYPE_BLE_HEADSET -> true
+            AudioDeviceInfo.TYPE_BUILTIN_EARPIECE -> true
             else -> false
         }
     }
